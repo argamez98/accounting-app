b8deac0ea24c238c0be480f4cf0a9f3ba05d1ea6
argamez98 <argamez98@gmail.com>
2021-08-10 12:19:40 -0500

    added a route to the main input page from the sign in page

---
+++ lib/screens/input_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFFCDB2),
+    );
+  }
+}
--- lib/screens/sign_in_page.dart
+++ lib/screens/sign_in_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
-class SignInPage extends StatelessWidget {
+import 'input_page.dart';
 
+class SignInPage extends StatelessWidget {
+  //elevated button style
   final elevatedButtonStyle = ElevatedButton.styleFrom(
     primary: Color(0xFF8777A6),
     minimumSize: Size(88,36),
@@ -29,7 +31,7 @@ class SignInPage extends StatelessWidget {
               child: Image.asset('web/icons/Icon-192.png'),
             ),
           ),
-          Padding(
+          Padding(  //USERNAME FORM
             padding: EdgeInsets.all(10),
             child: TextField(
               decoration: InputDecoration(
@@ -38,7 +40,7 @@ class SignInPage extends StatelessWidget {
                   hintText: 'Enter valid username'),
             ),
           ),
-          Padding(
+          Padding(  //PASSWORD FORM
             padding: EdgeInsets.all(10),
             child: TextField(
               obscureText: true,
@@ -48,10 +50,11 @@ class SignInPage extends StatelessWidget {
                   hintText: 'Enter your password'),
             ),
           ),
+          //login button that swaps to the input page
           ElevatedButton(
               style: elevatedButtonStyle,
               onPressed: () {
-                Navigator.push(context, route)
+                Navigator.push(context, MaterialPageRoute(builder: (context) => InputPage()));
               },
               child: Text("Login"))
         ],
