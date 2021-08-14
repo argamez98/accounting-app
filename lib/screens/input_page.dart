1d2c4e18986602ffbbc41dbd419125614df9da53
argamez98 <argamez98@gmail.com>
2021-08-13 20:46:09 -0500

    updated input page
    added a numpad to the input page, but does not display numbers onto the screen yet.

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:numeric_keyboard/numeric_keyboard.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -8,7 +9,8 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
-    
+    String text = "";
+
     final centerText = Center(
       child: Text(
         "Enter amount spent",
@@ -19,13 +21,44 @@ class _InputPageState extends State<InputPage> {
       ),
     );
 
-    final numDisplayBox = SizedBox(
-        width: MediaQuery.of(context).size.width / 1.4,
-        height: MediaQuery.of(context).size.height / 16,
-        child: DecoratedBox(
-            decoration: BoxDecoration(
-                color: Color(0xFFFFB4A2),
-                borderRadius: BorderRadius.circular(50))));
+    final numDisplayBox = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Spacer(),
+        SizedBox(
+            width: MediaQuery.of(context).size.width / 1.4,
+            height: MediaQuery.of(context).size.height / 16,
+            child: DecoratedBox(
+                decoration: BoxDecoration(
+                    color: Color(0xFFFFB4A2),
+                    borderRadius: BorderRadius.circular(50)))),
+        Spacer(),
+      ],
+    ); 
+    //helper function to work the buttons
+    _onKeyboardTap(String value) {
+      setState(() {
+        text = text + value;
+        print(text);
+      });
+    }
+
+    //NUMERIC KEYPAD
+    final numKeyboard = NumericKeyboard(
+      onKeyboardTap: _onKeyboardTap,
+      textColor: Colors.white,
+      rightButtonFn: () {
+        setState(() {
+          text = text.substring(0, text.length - 1);
+        });
+      },
+      rightIcon: Icon(Icons.backspace, color: Colors.white),
+      leftButtonFn: () {
+        print("left button clicked");
+      },
+      leftIcon: Icon(Icons.check, color: Colors.white),
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    );
 
     return Scaffold(
       backgroundColor: Color(0xFFFFCDB2),
@@ -34,6 +67,7 @@ class _InputPageState extends State<InputPage> {
         children: [
           centerText,
           numDisplayBox,
+          numKeyboard,
         ],
       ),
     );
